ea4024d58f64f78afc2df8dc46c21ccf20096b11
Pablo Rigazzi <p.rigazzi@coolblue.nl>
2018-03-30 21:48:58 +0200

    Schemas and Website Repositories working

--- packages/Website/src/Infrastructure/Persistence/GroupSqliteRepository.php
+++ packages/Website/src/Infrastructure/Persistence/GroupSqliteRepository.php
@@ -3,8 +3,32 @@ declare(strict_types=1);
 
 namespace Vonq\Website\Infrastructure\Persistence;
 
+use RuntimeException;
+use SQLite3;
 use Vonq\Website\Domain\Model\GroupRepositoryInterface;
 
 class GroupSqliteRepository implements GroupRepositoryInterface
 {
+    private $database;
+
+    public function __construct(string $databaseFile)
+    {
+        $this->database = new Sqlite3($databaseFile);
+        $this->createSchema();
+    }
+
+    private function createSchema()
+    {
+        $isValid = $this->database->exec("
+        CREATE TABLE IF NOT EXISTS VONQ_GROUPS (
+            group_group_id CHAR(36) PRIMARY KEY NOT NULL,
+            group_name VARCHAR(255) NOT NULL,
+            group_description VARCHAR(255) NOT NULL,
+            group_created_on DATETIME NOT NULL
+        )");
+
+        if (!$isValid) {
+            throw new RuntimeException("Couldn't create Group Table");
+        }
+    }
 }
--- packages/Website/src/Infrastructure/Persistence/UserSqliteRepository.php
+++ packages/Website/src/Infrastructure/Persistence/UserSqliteRepository.php
@@ -3,8 +3,32 @@ declare(strict_types=1);
 
 namespace Vonq\Website\Infrastructure\Persistence;
 
+use RuntimeException;
+use SQLite3;
 use Vonq\Website\Domain\Model\UserRepositoryInterface;
 
 class UserSqliteRepository implements UserRepositoryInterface
 {
+    private $database;
+
+    public function __construct(string $databaseFile)
+    {
+        $this->database = new Sqlite3($databaseFile);
+        $this->createSchema();
+    }
+
+    private function createSchema()
+    {
+        $isValid = $this->database->exec("
+        CREATE TABLE IF NOT EXISTS VONQ_USERS (
+            user_user_id CHAR(36) PRIMARY KEY NOT NULL,
+            user_group_id CHAR(36) NOT NULL,
+            user_name VARCHAR(255) NOT NULL,
+            user_created_on DATETIME NOT NULL
+        )");
+
+        if (!$isValid) {
+            throw new RuntimeException("Couldn't create User Table");
+        }
+    }
 }
